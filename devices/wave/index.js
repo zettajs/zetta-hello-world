606a9cf7d54a43d736b7b0cccd6c42f585f0e4db
Matt Dobson <mdobson4@gmail.com>
2014-10-03 09:36:07 -0400

    Finally got the sin wave looking cool as a cucumber.

--- devices/wave/index.js
+++ devices/wave/index.js
@@ -1,6 +1,10 @@
 var Device = require('zetta-device');
 var util = require('util');
 
+function degToRad(x) {
+  return x * ( Math.PI / 180 );
+}
+
 var WaveGenerator = module.exports = function() {
   Device.call(this);
   this.wave = 0;
@@ -14,10 +18,9 @@ WaveGenerator.prototype.init = function(config) {
     .monitor('wave');
 
   var self = this;
-  var vals = [-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1, 0.75, 0.5, 0.25, 0, -0.25, -0.5, -0.75, -1];
   var counter = 0;
   setInterval(function() {
-    self.wave = vals[counter % 17];
-    counter++;
-  }, 60);
+    self.wave = Math.sin(degToRad(counter));
+    counter += 15;
+  }, 100);
 };
