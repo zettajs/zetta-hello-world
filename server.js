70f51e4a4008919e9d937ab5433ab3e0724de40c
Adam Magaluk <AdamMagaluK@gmail.com>
2014-10-08 11:10:48 -0400

    Removed sine wave for step-1

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var zetta = require('zetta');
 var LED = require('zetta-mock-led');
-var Sine = require('zetta-sine-wave');
  
 zetta()
   .use(LED)
-  .use(Sine)
   .listen(1337, function(){
     console.log('Zetta is running at http://beaglebone.local:1337');
   });
