664e94781fc03dab1736152030d8c44374e3871f
Adam Magaluk <AdamMagaluK@gmail.com>
2014-10-08 11:11:47 -0400

    Removed everything for step-0

--- server.js
+++
@@ -1,8 +0,0 @@
-var zetta = require('zetta');
-var LED = require('zetta-mock-led');
- 
-zetta()
-  .use(LED)
-  .listen(1337, function(){
-    console.log('Zetta is running at http://beaglebone.local:1337');
-  });
