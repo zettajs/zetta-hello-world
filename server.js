776898c014da2128c2b49d6b70528589fee0840e
Adam Magaluk <AdamMagaluK@gmail.com>
2014-10-08 11:09:31 -0400

    Removed .link for step-2

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var Sine = require('zetta-sine-wave');
 zetta()
   .use(LED)
   .use(Sine)
-  .link('http://hello-zetta.herokuapp.com/')
   .listen(1337, function(){
     console.log('Zetta is running at http://beaglebone.local:1337');
   });
