0d3c3873d1ff542fe326cfc5e73ae3330a9c5791
Adam Magaluk <AdamMagaluK@gmail.com>
2014-10-08 11:08:49 -0400

    Removed app for step 3

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ var zetta = require('zetta');
 var LED = require('zetta-mock-led');
 var Sine = require('zetta-sine-wave');
  
-var app = require('./apps/app');
- 
 zetta()
   .use(LED)
   .use(Sine)
-  .use(app)
   .link('http://hello-zetta.herokuapp.com/')
   .listen(1337, function(){
     console.log('Zetta is running at http://beaglebone.local:1337');
