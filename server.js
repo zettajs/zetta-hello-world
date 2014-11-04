7759884b8a3a018b7364b989c04755c0961b3d18
landlessness <brian@landlessness.net>
2014-11-04 14:59:38 -0500

    sensor names conform to zetta std and app is named for dusk to dawn

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var zetta = require('zetta');
-var LED = require('zetta-mock-led');
-var Sine = require('zetta-sine-wave');
- 
-var app = require('./apps/app');
- 
+var LED = require('zetta-led-mock-driver');
+var Photocell = require('zetta-photocell-mock-driver');
+
+var duskToDawnLight = require('./apps/dusk_to_dawn_light');
+
 zetta()
+  .name('FirstName LastName')
   .use(LED)
-  .use(Sine)
-  .use(app)
+  .use(Photocell)
+  .use(duskToDawnLight)
   .link('http://hello-zetta.herokuapp.com/')
   .listen(1337, function(){
-    console.log('Zetta is running at http://beaglebone.local:1337');
-  });
+     console.log('Zetta is running at http://127.0.0.1:1337');
+});
